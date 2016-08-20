8677bd24d0355f87e8e2b2743ae458a400887f32
chingpo <jingbo15722@sina.com>
2016-08-20 15:16:38 +0800

    剑指offer

--- src/Solution.java
+++ src/Solution.java
@@ -11,27 +11,22 @@ import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Solution {
-    public static  int GetUglyNumber_Solution(int index) {
-    if(index<=0) return 0;
-    int[] result = new int[index];
-    int count=0;
-    int a=0,b=0,c=0,temp=0;
-    result[0]=1;
-    while(count<index-1){
-    	temp=min(result[a]*2,min(result[b]*3,result[c]*5));
-    	if(temp==result[a]*2) a++;
-    	if(temp==result[b]*3) b++;
-    	if(temp==result[c]*5) c++;
-    	result[++count]=temp;
-    }
-    return result[index-1];
-
-    }
-    private static  int min(int a,int b){
-    	return (a>b)?b:a;
+    public static  int FirstNotRepeatingChar(String str) {
+   if(str==null||str.length()==0) return -1;
+   ArrayList<Character> list=new ArrayList<Character>();
+   for(int i=0;i<str.length();i++){
+	   char ch=str.charAt(i);
+	   if(!list.contains(ch)){
+		   list.add(Character.valueOf(ch));
+	   }else{
+		   list.remove(Character.valueOf(ch));
+	   }
+   }
+   if(list.size()<=0) return -1;
+   return str.indexOf(list.get(0));
     }
     public static void main(String[] args) {
-    	System.out.println(GetUglyNumber_Solution(7));
+    	System.out.println(FirstNotRepeatingChar("google"));
     	
 	}
 }
